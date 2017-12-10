e7a13b0de970c552865dff1795e2e3b3d65274f7
Mauricio <mauricio.mina@correounivalle.edu.co>
2017-12-10 15:47:04 -0500

    organize project documentation

--- EngineSimilarityCosine/engines.py
+++ EngineSimilarityCosine/engines.py
@@ -30,20 +30,8 @@ class ContentEngine(object):
         info("Engine trained in %s seconds." % (time.time() - start))
 
     def _train(self, ds):
+        print(ds)
         """
-        Train the engine.
-
-        Create a TF-IDF matrix of unigrams, bigrams, and trigrams for each product. The 'stop_words' param
-        tells the TF-IDF module to ignore common english words like 'the', etc.
-
-        Then we compute similarity between all products using SciKit Leanr's linear_kernel (which in this case is
-        equivalent to cosine similarity).
-
-        Iterate through each item's similar items and store the 100 most-similar. Stops at 100 because well...
-        how many similar products do you really need to show?
-
-        Similarities and their scores are stored in redis as a Sorted Set, with one set for each item.
-
         :param ds: A pandas dataset containing two fields: description & id
         :return: Nothin!
         """
@@ -70,7 +58,7 @@ class ContentEngine(object):
         :return: A list of lists like: [["19", 0.2203], ["494", 0.1693], ...]. The first item in each sub-list is
         the item ID and the second is the similarity score. Sorted by similarity score, descending.
         """
-        print(self.r)
+
         return self._r.zrange(self.SIMKEY % item_id, 0, num-1, withscores=True, desc=True)
 
 
